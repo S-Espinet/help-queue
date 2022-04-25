4c2dfa27f8f9696f161a914c1c428142ae41351f
S. Espinet <s.lou.espinet@gmail.com>
2022-04-24 17:10:43 -0700

    update shared state functions to use redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
@@ -8,9 +9,11 @@ class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
+
+console.log(props);
+
     this.state = {
       formVisibleOnPage: false,
-      mainTicketList: [],
       selectedTicket: null,
       editing: false
     };
@@ -31,9 +34,17 @@ class TicketControl extends React.Component {
   }
 
   handleAddingNewTicketToList = (newTicket) => {
-    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
-    this.setState({mainTicketList: newMainTicketList,
-                  formVisibleOnPage: false });
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = newTicket;
+    const action = {
+      type: 'ADD_TICKET',
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    };
+    dispatch(action);
+    this.setState({formVisibleOnPage: false});
   }
 
   handleChangingSelectedTicket = (id) => {
@@ -42,9 +53,13 @@ class TicketControl extends React.Component {
   }
 
   handleDeletingTicket = (id) => {
-    const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
+    const { dispatch } = this.props;
+    const action = {
+      type: 'DELETE_TICKET',
+      id: id
+    };
+    dispatch(action);
     this.setState({
-      mainTicketList: newMainTicketList,
       selectedTicket: null
     });
   }
@@ -55,11 +70,17 @@ class TicketControl extends React.Component {
   }
 
   handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = this.state.mainTicketList
-      .filter(ticket => ticket.id !== this.state.selectedTicket.id)
-      .concat(ticketToEdit);
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = ticketToEdit;
+    const action = {
+      type: 'ADD_TICKET',
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    };
+    dispatch(action);
     this.setState({
-        mainTicketList: editedMainTicketList,
         editing: false,
         selectedTicket: null
       });
@@ -97,5 +118,7 @@ class TicketControl extends React.Component {
     );
   }
 }
+
+TicketControl = connect()(TicketControl);
 
 export default TicketControl;
